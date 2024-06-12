a0e9e00e4d7bf5d92d6d2294cabb6e8badd8ea70
SK YASIN BUX <154232291+skyasinbux@users.noreply.github.com>
2024-06-12 17:00:19 +0530

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   let date = new Date();
@@ -38,7 +38,7 @@ function Footer() {
                 target="_blank" 
                 rel="noopener noreferrer"
               >
-                <AiOutlineTwitter />
+                <FaXTwitter />
               </a>
             </li>
             <li className="social-icons">
