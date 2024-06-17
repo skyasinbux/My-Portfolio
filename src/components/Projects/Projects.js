7ec36358c8b0d08f839aaaff914f42f59635f796
SK YASIN BUX <154232291+skyasinbux@users.noreply.github.com>
2024-06-17 14:04:55 +0530

    Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,8 +28,8 @@ function Projects() {
               isBlog={false}
               title="Nextxen Learning"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="/"
+              demoLink="/"
             />
           </Col>
           <Col md={4} className="project-card">
