40b1a5e9bf5aa9a79edb87c6fee01b183d26b07a
SK YASIN BUX <154232291+skyasinbux@users.noreply.github.com>
2024-06-22 03:10:23 +0530

    ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/My_resume_yasin.pdf";
+import pdf from "../../Assets/../Assets/My_resume_updated.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
