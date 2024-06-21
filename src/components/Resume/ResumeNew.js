8667bb1977ec30aa99d5bbdc7276352ec42e8bd7
SK YASIN BUX <154232291+skyasinbux@users.noreply.github.com>
2024-06-22 03:15:07 +0530

    ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -36,6 +36,9 @@ function ResumeNew() {
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+            Download to see full CV
+        </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
