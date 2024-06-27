e3ef360db8473105cf6991a6ba34dba43f8d13a9
SK YASIN BUX <154232291+skyasinbux@users.noreply.github.com>
2024-06-27 07:49:45 +0530

    Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -91,16 +91,6 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="/"
-                // target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
             <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/skyasinbux/My-Portfolio"
